24e73323c6dfa0fc1624c9e5effa7b67301d0140
AloraTab <alorartabuco@gmail.com>
2024-12-25 17:17:51 +0400

    Build error for &apos; fixed

--- app/components/MyProfile.tsx
+++ app/components/MyProfile.tsx
@@ -19,7 +19,7 @@ const MyProfile = () => {
 
                     <div className="relative w-[203px] h-[200px] md:w-[406px] md:h-[398px] mb-6 md:mb-0 md:mr-8 z-10">
                         <Image
-                            src={'/img/profile.svg'}
+                            src={&apos;/img/profile.svg&apos;}
                             alt="Profile Picture"
                             width={406}
                             height={398}
@@ -37,9 +37,9 @@ const MyProfile = () => {
                     <b className="text-xl">My name is Alora Tabuco.</b>
                     <p className="text-lg">
                         <br /><br />
-                        I'm an AI/ML software engineer with a passion for transforming complex AI concepts into practical solutions. Through hands-on experience and technical expertise, I've developed AI applications that deliver real value for clients, stakeholders, and hackathon projects.
+                        I&apos;m an AI/ML software engineer with a passion for transforming complex AI concepts into practical solutions. Through hands-on experience and technical expertise, I&apos;ve developed AI applications that deliver real value for clients, stakeholders, and hackathon projects.
                         <br /> <br />
-                        I graduated in 2023 with a BSc in Computer Science, specializing in Artificial Intelligence. After gaining valuable experience as an AI/ML Software Engineer, I'm now seeking new opportunities to grow and contribute to innovative projects that push the boundaries of what's possible with AI.
+                        I graduated in 2023 with a BSc in Computer Science, specializing in Artificial Intelligence. After gaining valuable experience as an AI/ML Software Engineer, I&apos;m now seeking new opportunities to grow and contribute to innovative projects that push the boundaries of what&apos;s possible with AI.
                          <br /> <br />
                          My focus lies in building AI systems that are both powerful and practical. I believe in creating solutions that not only leverage cutting-edge technology but also address real-world challenges in accessible and meaningful ways.
                     </p>
